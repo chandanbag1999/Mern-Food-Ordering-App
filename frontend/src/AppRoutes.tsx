4f0f0fb5b8386772e8c65f31caf246e499f57f08
Chandan Bag <cbag67612@gmail.com>
2024-06-07 20:56:47 +0530

    Add Responsive Landing page and build home page

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -1,10 +1,11 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Layout from './layouts/layout';
+import Home from './pages/Home';
 
 export default function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Layout>Home Page</Layout>} />
+      <Route path="/" element={<Layout><Home /></Layout>} />
       <Route path="/user-profile" element={<span>USER PROFILE</span>} />
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
