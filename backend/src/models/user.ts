c254ce70e3330e4b914552c83a8e5bfad7de0851
Chandan Bag <cbag67612@gmail.com>
2024-06-09 20:09:11 +0530

    create user schema in model

---
+++ backend/src/models/user.ts
@@ -0,0 +1,26 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema({
+  auth0Id: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+  },
+  addressLine1: {
+    type: String,
+  },
+  city: {
+    type: String,
+  },
+  country: {
+    type: String,
+  }
+},{timestamps: true});
+
+export const User = mongoose.model("User", userSchema);
