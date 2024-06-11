5e49b816618698db65a9d77b76e5ee3d3591a236
Chandan Bag <cbag67612@gmail.com>
2024-06-11 15:02:11 +0530

    Add accessToken

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Layout from './layouts/layout';
 import Home from './pages/Home';
+import AuthCallbackPage from './pages/AuthCallbackPage';
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Layout><Home /></Layout>} />
+      <Route path="/auth-callback" element={<AuthCallbackPage />} />
       <Route path="/user-profile" element={<span>USER PROFILE</span>} />
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
--- frontend/src/api/userApi.tsx
+++ frontend/src/api/userApi.tsx
@@ -1,3 +1,4 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import { useMutation } from "react-query";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
@@ -8,10 +9,14 @@ type createUserRequest = {
 };
 
 export const useCreateUser = () => {
+  const { getAccessTokenSilently } = useAuth0();
+
   const createUserRequest = async (user: createUserRequest) => {
+    const access_token = await getAccessTokenSilently();
     const response = await fetch(`${API_BASE_URL}/api/v1/users`, {
       method: "POST",
       headers: {
+        Authorization: `Bearer ${access_token}`,
         "Content-Type": "application/json",
       },
       body: JSON.stringify(user),
