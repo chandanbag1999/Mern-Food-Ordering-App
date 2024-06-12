2fb779aa5f59daf1a08b3914fdc34f01279363bd
Chandan Bag <cbag67612@gmail.com>
2024-06-12 21:54:17 +0530

    create helth check up api route and create build & start command in package.json

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,6 +13,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get("/health", (req: Request, res: Response) => {
+  res.send({ message: "health Ok!" });
+});
 
 app.use("/api/v1/users", myUserRoute)
 
