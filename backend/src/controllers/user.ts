59072c3840de3b1ea233e6bdc8c04041cacb85db
Chandan Bag <cbag67612@gmail.com>
2024-06-12 19:43:32 +0530

    complete build get current user Api route

--- backend/src/controllers/user.ts
+++ backend/src/controllers/user.ts
@@ -49,3 +49,18 @@ export const updateUser = async (req: Request, res: Response) => {
   }
 };
 
+export const getCurrentUser = async (req: Request, res: Response) => {
+  try {
+    const currentUser = await User.findById(req.userId);
+
+    if (!currentUser) {
+      return res.status(404).json({ error: "User not found" });
+    };
+
+    res.status(200).json(currentUser.toObject());
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Error getting user" });
+  };
+};
+
--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { createUser, updateUser } from "../controllers/user";
+import { createUser, getCurrentUser, updateUser } from "../controllers/user";
 import { jwtCheck, jwtParse } from "../middleware/auth";
 import { validateUserProfile } from "../middleware/validation";
 
 const router = Router();
 
+router.route("/").get(jwtCheck, jwtParse, getCurrentUser);
 router.route("/").post(jwtCheck, createUser)
 router.route("/profile-update").put(
   jwtCheck, 
